9517f7debc40928651970b088e3fa4ab2013b9dc
Brijesh verma <bv2611999@gmail.com>
2024-07-01 19:11:35 +0530

    Update 3.Zero Matrix.cpp

--- 3.Zero Matrix.cpp
+++ 3.Zero Matrix.cpp
@@ -1,21 +1,37 @@
 /******************************************************************************
-Zero Matrix: Write an algorithm such that if an element in an MxN matrix is 0, its entire row and
-column are set to 0. 
+String Rotation:Assumeyou have a method isSubstringwhich checks if oneword is a substring
+of another. Given two strings, sl and s2, write code to check if s2 is a rotation of sl using only one
+call to isSubstring (e.g., "waterbottle" is a rotation of"erbottlewat"). 
 *******************************************************************************/
 
 #include<bits/stdc++.h>
 using namespace std;
 void rotateMatrix(vector<vector<int>>v){
+    vector<bool>row(4);
+    vector<bool>col(4);
     for(int i=0; i<4; i++){
         for(int j=0; j<4; j++){
             if(v[i][j]==0){
-                int col=0;
-                while(col<4){
-                    v[i][col++]=0;
-                }
+                row[i]=true;
+                col[j]=true;
             }
         }
     }
+    // row zero
+    for(int i=0; i<4; i++){
+        if(!row[i]) continue;
+        for(int j=0; j<4; j++){
+            v[i][j]=0;
+        }
+    }
+    // col zero
+    for(int i=0; i<4; i++){
+        if(!col[i]) continue;
+        for(int j=0; j<4; j++){
+            v[j][i]=0;
+        }
+    }
+    
     for(int i=0; i<4; i++){
         for(int j=0; j<4; j++)  cout << v[i][j]<<" ";
         cout << endl;
